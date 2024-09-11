2c168b25a5b949b4fd5d07ef573d7ceea0d06d49
Bowhle <bowhlem@gmail.com>
2024-09-12 01:40:47 +0200

    There are only 3 colors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    # Compute the last digit
-    last_digit = abs(number) % 10
-    # Handle negative numbers by adjusting the sign of the last digit
     if number < 0:
-        last_digit = -last_digit
-    # Print the last digit
-    print(last_digit, end="")
-    # Return the last digit
-    return last_digit
+        number = -number
+    last_digit = number % 10
+    print("{}".format(last_digit), end='')
+    return (last_digit)

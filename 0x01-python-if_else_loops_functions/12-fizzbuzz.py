50c476e212220128fc8dbba2b2c2d0657ef6abc1
Bowhle <bowhlem@gmail.com>
2024-09-12 21:14:25 +0200

    I've got this!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,16 +8,12 @@ Not allowed to use import modules
 Use prototype: def fizzbuzz():
 """
 def fizzbuzz():
-    # The FizzBuzz function only prints once
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=' ')
-        elif i % 3 == 0:
-            print("Fizz", end=' ')
-        elif i % 5 == 0:
-            print("Buzz", end=' ')
+        if i % 3 == 0 and i % 5 != 0:
+            print("Fizz ", end='')
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz ", end='')
+        elif i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz ", end='')
         else:
-            print(i, end=' ')
-
-# Call it once, and only once
-fizzbuzz()
+            print("{} ".format(i), end='')

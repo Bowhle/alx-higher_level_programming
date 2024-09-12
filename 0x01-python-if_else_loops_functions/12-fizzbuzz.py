1ee9c0cf8c5edac6944783ef850617f4de65aba1
Bowhle <bowhlem@gmail.com>
2024-09-12 21:16:43 +0200

    I've got this2!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """
 Iterate numbers from 1 to 100 separated by space
-For MULTIPLES od 3 PRINT FIZZ
+For MULTIPLES of 3 PRINT FIZZ
 For MULTIPLES of 5 PRINT BUZZ
 For MULTIPLES OF BOTH PRINT FIZZBUZZ
 Not allowed to use import modules
 Use prototype: def fizzbuzz():
 """
+
+
 def fizzbuzz():
-
-
+    """Prints numbers from 1 to 100 with FizzBuzz rules."""
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 != 0:
-            print("Fizz ", end='')
-        elif i % 5 == 0 and i % 3 != 0:
-            print("Buzz ", end='')
-        elif i % 5 == 0 and i % 3 == 0:
-            print("FizzBuzz ", end='')
+        if i % 15 == 0:
+            print("FizzBuzz", end=' ')
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
         else:
-            print("{} ".format(i), end='')
+            print("{}".format(i), end=' ')
+
+
+# Call the fizzbuzz function
+fizzbuzz()
+

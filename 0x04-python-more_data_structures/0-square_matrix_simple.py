5aef1aada690df62a2278e4bafcf80a48959673c
bowhle <bowhlem@gmailcom>
2024-09-28 09:17:51 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,5 @@
-def square_matrix_simple(matrix=[]):
-    """
-    Compute the square value of all integers in a 2D matrix.
+#!/usr/bin/python3
+from typing import List, Any
 
-    Args:
-        matrix: A 2D matrix (list of lists).
-
-    Returns:
-        A new matrix with the same size where each value is squared.
-    """
-    # Using a list comprehension to create a new matrix with squared values.
-    return [[x**2 for x in row] for row in matrix]
-
+def search_replace(my_list: List[Any], search: Any, replace: Any) -> List[Any]:
+    return [replace if item == search else item for item in my_list]

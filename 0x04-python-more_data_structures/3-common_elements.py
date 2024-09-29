aa5aaf63e2725ec026032bf27fdec29c98ab7636
bowhle <bowhlem@gmailcom>
2024-09-29 02:55:45 -0700

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+
+    Args:
+        set_1: First set of elements.
+        set_2: Second set of elements.
+
+    Returns:
+        A set containing common elements from set_1 and set_2.
+    """
+    return set_1 & set_2

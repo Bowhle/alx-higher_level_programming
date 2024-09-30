40dff29b1efc52374aac1222ab028c014019a379
Bowhle <bowhlem@gmail.com>
2024-10-01 01:35:08 +0200

    Print and count integers debug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python4
 
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
     count = 0
     try:
         print("{:d}".format(my_list[x]), end="")

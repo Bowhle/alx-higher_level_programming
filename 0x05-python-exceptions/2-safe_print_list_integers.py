71f7225c09092bba268cc075f33adec785d36d27
Bowhle <bowhlem@gmail.com>
2024-10-01 01:37:58 +0200

    debug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python4
 
 def safe_print_list_integers(my_list=None, x=0):
-    if my_list is None:
-        my_list = []
     count = 0
+    for i in range(x):
     try:
         print("{:d}".format(my_list[x]), end="")
         count += 1

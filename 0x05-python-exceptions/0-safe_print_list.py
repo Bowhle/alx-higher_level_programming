64b4a1c6f396b68bcf5827d6f0dac3108250a1b2
Bowhle <bowhlem@gmail.com>
2024-10-01 01:19:34 +0200

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-Module Name: Safe list printing
-
-Description: Writes a function that prints x elements of a list
-"""
-
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
     count = 0
     try:
         for i in range(x):

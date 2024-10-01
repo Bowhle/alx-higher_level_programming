1338d0f21fb18626a65b7c50f14e52a3f98cde48
bowhle <bowhlem@gmailcom>
2024-10-01 08:35:16 -0700

    more conflicts

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,14 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
             count += 1
         except (ValueError, TypeError):
-<<<<<<< HEAD
             pass
         except IndexError:
             break
     print()
-=======
-            continue
-    print("")
->>>>>>> 062a8788f3da7b0d968d9a6abb2b5988d9eb1c37
     return count
-

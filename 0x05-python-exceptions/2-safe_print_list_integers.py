5670968487183b0c1ec2cc666b44289203e692c0
Bowhle <bowhlem@gmail.com>
2024-10-02 05:56:26 +0200

    safe print with debug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
     count = 0
     for i in range(x):
         try:
@@ -11,4 +13,3 @@ def safe_print_list_integers(my_list=[], x=0):
             break
     print()
     return count
-

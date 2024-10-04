ce512c11ecbc87dcab1552fcc689bc7803427053
Bowhle <bowhlem@gmail.com>
2024-10-04 17:46:10 +0200

    Area of a square 2

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,9 +15,6 @@ class Square():
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """
-            Calculate and return the area of the square.
-            Returns an int area of the square.
-            """
-            return self.__size ** 2
+    def area(self):
+        """Public method: computes the area of the class object"""
+        return (self.__size * self.__size)

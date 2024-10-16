54e40ca4a64e6123e20dbc7aee26b1b42ea57751
bowhle <bowhlem@gmailcom>
2024-10-16 17:50:32 +0200

    Debugging 6

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,9 @@ def save_to_json_file(my_obj, filename):
         filename (str): The name of the file where the JSON representation
                         will be written.
     """
+    # Convert set to list if my_obj is a set
+    if isinstance(my_obj, set):
+        my_obj = list(my_obj)
+
     with open(filename, mode='w', encoding='utf-8') as a_file:
         json.dump(my_obj, a_file)  # Directly dump the JSON object to the file.

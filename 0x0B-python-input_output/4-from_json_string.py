5d264b002bf8e61dbdc347a7499002aee6a98fb3
bowhle <bowhlem@gmailcom>
2024-10-16 17:35:02 +0200

    From JSON string to Object Debug 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-"""A function that returns an object
-(Python data structure) from a JSON string.
-"""
+"""A function that returns an object(Python data structure) from a JSON string."""
 import json
-"""Using the json module as we will be converting 
-the JSON string to a Python data structure.
-"""
+"""Using the json module as we will be converting the JSON string to a Py data structure."""
 
 
 def from_json_string(my_str):
-    """Convert a JSON string to a Python data structure.
-
-    Args:
-        my_str (str): The JSON string to be converted.
-
-    Returns:
-        object: The Python data structure represented by the JSON string.
-    """
+    """This function collects a json string and gives it a python data structure"""
     return json.loads(my_str)

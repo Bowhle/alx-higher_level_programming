db8e9ec78d1fa02e20ae9e0ef2bbfa3145884cfb
bowhle <bowhlem@gmail.com>
2024-12-16 15:26:47 +0200

    Arguments3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* This script prints a message depending of the number of arguments passed */
 
-const args = process.argv.slice(2);
+const args = process.argv;
 
 if (args.length === 0)
     {

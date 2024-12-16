dadea50e7743d07513a9ae5c386d6921cb9777e7
bowhle <bowhlem@gmail.com>
2024-12-16 19:27:50 +0200

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* This script prints two arguments passed to it, in the following format: “ is ” */
 
-const args1 = process.argv[2];
-const args2 = process.argv[3];
+const arg1 = process.argv[2] || 'undefined';
+const arg2 = process.argv[3] || 'undefined';
 
-console.log(args1 + ' is ' + args2);
+console.log(arg1 + ' is ' + arg2);

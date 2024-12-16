425fee6d60147b63549ba32503d4dc20ae6e18e6
bowhle <bowhlem@gmail.com>
2024-12-16 15:27:30 +0200

    Arguments4

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 /* This script prints a message depending of the number of arguments passed */
 
-const args = process.argv;
+const argv = process.argv;
 
-if (args.length === 0)
-    {
-    console.log('No argument');
-    }
-else if (args.length === 1)
-    {
-    console.log('Argument found');
-    }
-else
-    {
-    console.log('Arguments found');
-    }
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

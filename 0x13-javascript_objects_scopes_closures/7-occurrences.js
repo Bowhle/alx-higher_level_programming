4f1b4ad2d8d3a0848a2eb4ce5e00a5af124fcd99
bowhle <bowhlem@gmail.com>
2025-01-05 20:55:16 +0200

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/** Function that returns the number
+of occurrences of an element in a list */
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};

b93c52beaf9a29f014cfb268481ee96a67e311a3
bowhle <bowhlem@gmail.com>
2025-01-05 21:00:30 +0200

    Number conversion

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/** Function to convert a number from base 10
+to another base */
+exports.converter = function (base) {
+  // The number to be converted is 100 (base 10).
+  console.log((100).toString(base));
+};

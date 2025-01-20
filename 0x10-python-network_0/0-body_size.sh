5ccdef7239ac4472ab815cee8a81524146d7f78b
bowhle <bowhlem@gmail.com>
2025-01-20 05:29:18 +0200

    cURL Method

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request, and displays the body size in bytes.
-# The -s option silences the output,
-# and -w is used to print the size of the response body
 curl -s -w '%{size_download}\n' -o /dev/null "$1"

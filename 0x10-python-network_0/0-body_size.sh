10c6372e03b767e1bcba989ab2afd78c96c5bbfd
bowhle <bowhlem@gmail.com>
2025-01-20 05:17:38 +0200

    cURL to the end

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Takes in a URL, sends a request, and displays the body size in bytes.
 # The -s option silences the output,
 # and -w is used to print the size of the response body
-curl -s -w '%{size_download}\n' -o /dev/null "$1"
+curl -s "$1" | wc -c

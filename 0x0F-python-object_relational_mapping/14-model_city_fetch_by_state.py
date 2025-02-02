38c7a2c66411ecb847ab3798af8c1ea418a2ad4c
bowhle <bowhlem@gmail.com>
2025-02-02 14:48:19 +0200

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""  a script that lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa """
+"""
+A script that lists all City objects from the database hbtn_0e_14_usa.
+"""
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_city import City
 from model_state import State
-import sys
 
 if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     db = sys.argv[3]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(user, passwd, db), pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(user, passwd, db),
+        pool_pre_ping=True
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State)\
-            .filter(City.state_id == State.id).order_by(City.id):
+    results = session.query(City, State).filter(City.state_id == State.id).order_by(City.id)
+
+    for city, state in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()

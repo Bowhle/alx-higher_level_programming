2ddff081a7bff0a6eff0f0948fdaeaa5ad5bac29
bowhle <bowhlem@gmail.com>
2025-02-02 15:04:27 +0200

    Cities in state 2

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,8 +5,8 @@ A script that lists all City objects from the database hbtn_0e_14_usa.
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_city import City
 from model_state import State
+from model_city import City
 
 if __name__ == "__main__":
     user = sys.argv[1]

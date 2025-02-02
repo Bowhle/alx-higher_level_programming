ea5535969058badf86ca4f2c88972d9f70c46728
bowhle <bowhlem@gmail.com>
2025-02-02 14:17:30 +0200

    Delete states 4

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Delete all State objects with a name containing the letter a."""
+"""
+Delete all State objects with a name containing
+the letter a.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
